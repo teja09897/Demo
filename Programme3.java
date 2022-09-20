d4bee61861301b78d64e31b825877f76f6638d75
teja09897 <113973314+teja09897@users.noreply.github.com>
2022-09-20 18:44:33 +0530

    Update Programme3.java

--- Programme3.java
+++ Programme3.java
@@ -1,5 +1,6 @@
 class Programme3{
    public static void main(String[] args){
      Sysstem.out.println("Third programme added to github");
+      Sysstem.out.println(" programme added to github and changed");
    }
 }
